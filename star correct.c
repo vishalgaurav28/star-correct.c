4055826602b694cb3e15e16faf64bf373e111a71
vishalgaurav28 <67232758+vishalgaurav28@users.noreply.github.com>
2020-07-27 00:23:34 -0700

    Create star correct.c

---
+++ star correct.c
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("no of column\n");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+    for(i=1;i<=2;i++)
+    {
+       for(int j=1;j<=2;j++)
+       {
+           printf("*");
+           printf(" ");
+       }
+       printf("\n");
+}
+for(int i=1;i<=n;i++)
+{
+    printf("#");
+}
+}
